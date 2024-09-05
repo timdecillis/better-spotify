73d65b279cbcf6034660a84707d106567ad1d07f
timdecillis <timdecillis@gmail.com>
2024-09-04 21:10:14 -0700

    code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h3>Spotify, but better</h3>
       <div>
-        {songs.map((song, i) => (
-          <div key={i}>{song.title}</div>
-        ))}
+        {songs.map((song, i) => {
+          const { artist, title } = song;
+          return (
+            <div key={i}>
+              {artist}: {title}
+            </div>
+          );
+        })}
       </div>
     </main>
   );
