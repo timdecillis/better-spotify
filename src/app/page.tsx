f22a30562b19e97c0975928b8c0525ef84933415
timdecillis <timdecillis@gmail.com>
2024-09-04 21:05:15 -0700

    rendered song titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { user } from "./dummyData";
 
+const { songs } = user;
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h3>Spotify, but better</h3>
-
+      <div>{songs.map((song) => <div>{song.title}</div>)}</div>
     </main>
   );
 }
