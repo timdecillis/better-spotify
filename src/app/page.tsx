55eccc5736988681347697a484d95d73f4cb66ba
timdecillis <timdecillis@gmail.com>
2024-09-04 21:06:12 -0700

    added key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h3>Spotify, but better</h3>
-      <div>{songs.map((song) => <div>{song.title}</div>)}</div>
+      <div>
+        {songs.map((song, i) => (
+          <div key={i}>{song.title}</div>
+        ))}
+      </div>
     </main>
   );
 }
