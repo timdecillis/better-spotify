1b1110f27318dfc37aa901899aa70a7c5888cfb7
timdecillis <timdecillis@gmail.com>
2024-09-04 21:01:49 -0700

    imported dummy data

--- src/app/dummyData.ts
+++ src/app/dummyData.ts
@@ -1,4 +1,4 @@
-const user = {
+export const user = {
   name: "username",
   songs: [
     {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { user } from "./dummyData";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h3>Spotify, but better</h3>
+
     </main>
   );
 }
