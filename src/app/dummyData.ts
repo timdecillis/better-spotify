ee0bf06ff38e02e8ad7ec3fab40c6e2d446d70e4
timdecillis <timdecillis@gmail.com>
2024-09-04 21:00:53 -0700

    created dummy data

---
+++ src/app/dummyData.ts
@@ -0,0 +1,17 @@
+const user = {
+  name: "username",
+  songs: [
+    {
+      artist: "terrace martin",
+      title: "beige",
+    },
+    {
+      artist: "kendrick lamar",
+      title: "how much a dollar cost",
+    },
+    {
+      artist: "thundercat",
+      title: "drank",
+    },
+  ],
+};
